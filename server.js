02df496fbc841ee2c10277aeb703cf36275e93dd
cbartholomew69 <bartholomew_christy@yahoo.com>
2018-02-19 13:47:55 -0500

    modify server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express'),
+var express = require('express');
+var path = require('path')
+var app = express();
 app = express();
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
