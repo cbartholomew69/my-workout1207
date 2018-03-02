7fdbc97ced0170133aefb50ab76d6c65fc1ca488
cbartholomew69 <bartholomew_christy@yahoo.com>
2018-03-02 14:44:05 -0500

    create onSubmit function

--- src/app/services/workout.service.ts
+++ src/app/services/workout.service.ts
@@ -18,4 +18,11 @@ export class WorkoutService {
         return this.http.get(this.workoutsUrl+'?apiKey='+this.apiKey)
             .map(res => res.json());
     }
+
+    addWorkout(workout){
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.post(this.workoutsUrl+'?apiKey='+this.apiKey, JSON.stringify(workout), {headers: headers})
+            .map(res => res.json());
+    }
 }
--- src/pages/workouts/workouts.ts
+++ src/pages/workouts/workouts.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WorkoutService } from '../../app/services/workout.service';
 import { WorkoutDetailsPage } from '../workout-details/workout-details';
+
 @Component({
   selector: 'workouts',
   templateUrl: 'workouts.html'
