499f8aa1fa3a63dcc26d44a76cc8a2692d15443e
cbartholomew69 <bartholomew_christy@yahoo.com>
2018-03-05 14:42:11 -0500

    add delete button

--- src/pages/workouts/workouts.ts
+++ src/pages/workouts/workouts.ts
@@ -22,11 +22,17 @@ export class WorkoutsPage {
 
   }
 
+  ionViewWillEnter() {
+    this.workoutService.getWorkouts().subscribe(workouts => {
+      this.workouts = workouts;
+    });
+  }
+
   workoutSelected(event, workout) {
     this.navCtrl.push(WorkoutDetailsPage, {
       workout: workout
     });
-
+  
   }
 
 }
